d2e4c4a3e08d82f85f9a98b7bf0a8c8752f65bc3
Muto93 <Mahmut.Cinar93@outlook.de>
2023-10-14 20:31:51 +0200

    Implement game()

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function playRound(playerSelection, computerSelection){
     else if ((formatPlayerSelection === 'Paper' && computerSelection === 'Rock')
     || (formatPlayerSelection === 'Rock' && computerSelection === "Scissors")
     || (formatPlayerSelection === 'Scissors' && computerSelection === 'Paper')){
-        gameResult = `You won! ${formatPlayerSelection} beats ${computerSelection}`;
+        gameResult = `You win! ${formatPlayerSelection} beats ${computerSelection}`;
     }
     else {
         gameResult = `You lose! ${computerSelection} beats ${formatPlayerSelection}`;
@@ -39,3 +39,29 @@ function playRound(playerSelection, computerSelection){
 
 //game(play) 5 rounds console.log for each round winner get Input through prompt()
 
+function game (){
+    let playerSelection
+    ,   computerSelection
+    ,   playerPoints = 0
+    ,   computerPoints = 0
+    ,   gameResult;
+
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("Time to pick! Rock, Paper or Scissors?");
+        computerSelection = getComputerChoice();
+        gameResult = playRound(playerSelection, computerSelection);
+        if(gameResult.includes("win")) playerPoints++;
+        else if(gameResult.includes("lose")) computerPoints ++;
+        console.log(gameResult);
+        console.log(`Standing: Player ${playerPoints} - Computer ${computerPoints}`);
+    }
+    if(playerPoints > computerPoints){
+        console.log(`You are the winner with a final score of ${playerPoints} to ${computerPoints} Congratulations!!`);
+    }
+    else if(playerPoints < computerPoints){
+        console.log(`You lose with a final score of ${playerPoints} to ${computerPoints} Unlucky!!`);
+    }
+    else {
+        console.log("It's a draw! Well played everyone!");
+    }
+}
